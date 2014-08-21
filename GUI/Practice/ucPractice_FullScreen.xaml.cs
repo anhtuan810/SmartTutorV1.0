b8389ea16409372087d4a7093c0a4a53250a9ece
anhtuan810 <anhtuan.mta@gmail.com>
2014-08-21 18:03:49 +0100

    Fixed a bit of GUI

---
+++ GUI/Practice/ucPractice_FullScreen.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+	/// <summary>
+	/// Interaction logic for ucPractice.xaml
+	/// </summary>
+	public partial class ucPractice_FullScreen : UserControl
+	{
+        public ucPractice_FullScreen()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
